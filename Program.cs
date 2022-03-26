4e66f4588ddf83228e1cf9b563bd5123bff001bf
JoaoPedroCordoba <jp.8pedro86@gmail.com>
2022-03-25 22:09:18 -0300

    finalizadoexercicio

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿decimal salario, fgts;
+
+Console.WriteLine("Por favor, digite seu salario:");
+salario = Convert.ToDecimal(Console.ReadLine());
+
+fgts = (salario * 0.08m);
+
+Console.WriteLine($"FGTS:{fgts:C2}");
